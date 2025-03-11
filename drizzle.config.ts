368264393aee8f07eeaecebb8a4a274e29d79702
felix <felix@oakz.org>
2025-03-11 11:37:44 -0300

    back to .js

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import { constants } from './src/configs/constants'
 
 export default defineConfig({
-  schema: "./src/models/**/*.ts",
+  // schema: "./src/models/**/*.ts",
+  schema: "./dist/models/**/*.js",
   out: "./src/migrations",
   dialect: "postgresql",
   dbCredentials: {
