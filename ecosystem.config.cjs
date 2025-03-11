38b42c9b6f1cabfedc370628033cb277359d6f00
felix <felix@oakz.org>
2025-03-11 16:27:53 -0300

    adding prod env variable

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,9 +4,16 @@ module.exports = {
       name: 'daemonoakz_server',
       script: 'dist/server.js',
       instances: 1,
+      autorestart: true,
       exec_mode: 'fork', // To ensure one process
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
       watch: true,       // Watch file changes
-      ignore_watch: ['src/migrations'],  // Avoid watching migrations
+      ignore_watch: ['node_modules', 'logs', 'tmp', 'src/migrations'],
       watch_options: {
         usePolling: true  // Enable polling to ensure that file changes are detected
       }
