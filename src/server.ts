6bcedf73c6df8d3f2e39227b4776d109df7cbce7
felix <felix@oakz.org>
2025-03-17 08:17:22 -0300

    adds env prod log

--- src/server.ts
+++ src/server.ts
@@ -50,6 +50,7 @@ routes.forEach(({ route, prefix }) => fastify.register(route, { prefix }));
 const start = async (): Promise<void> => {
   try {
     await fastify.listen({ port });
+    console.log(`prod? `, process.env.NODE_ENV === 'production')
     console.log(`===> Server is running on http://localhost:${port}`);
 
   } catch (err) {
