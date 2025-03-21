ec217246924659e42ee2cc07aa7656ecdb42d364
felix <felix@oakz.org>
2025-03-21 11:22:46 -0300

    adds clear cookie when unauthorized

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -6,6 +6,14 @@ export async function authMiddleware(request: FastifyRequest, reply: FastifyRepl
 
   } catch (err) {
     console.log('authMiddleware Error:\n', err);
+
+    reply.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    });
+
     reply.status(401).send({ error: "Unauthorized" });
 
   }
